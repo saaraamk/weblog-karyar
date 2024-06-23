4c4917fa747c3d0e7bc104f2f7f03c6a0a401cb8
Sara <saramohammadikahvarin@gmail.com>
2024-06-23 15:58:35 +0330

    add two more models: author and like

--- posts/admin.py
+++ posts/admin.py
@@ -1,12 +1,28 @@
 from django.contrib import admin
-from .models import Post, Comment
+from .models import Post, Comment, Author, Like
 
-class CommentAdminInLine(admin.TabularInline):
-    model = Comment
-    feilds = ["text", "created_at"]
+
+# class CommentAdminInLine(admin.TabularInline):
+#     model = Comment
+#     feilds = ["text", "created_at"]
+
+# class PostAdmin(admin.ModelAdmin):
+#     list_display =["id","title", "created_at"]
+#     inlines = [CommentAdminInLine]
+
+# admin.site.register(Post,PostAdmin)
+
+class AuthorAdmin(admin.ModelAdmin):
+    feilds = ["user_name", "created_at"]
+
+admin.site.register(Author, AuthorAdmin)
+
 
 class PostAdmin(admin.ModelAdmin):
-    list_display =["id","title", "created_at"]
-    inlines = [CommentAdminInLine]
+    feilds = ["text", "created_at"]
 
-admin.site.register(Post,PostAdmin)
+admin.site.register(Post, PostAdmin)
+
+admin.site.register(Comment)
+
+admin.site.register(Like)
--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 
+class Author(models.Model):
+    user_name = models.CharField(max_length=50)
+    bio = models.TextField(unique=True)
+    email = models.EmailField()
+    created_at = models.DateField()
+
+    def __str__(self) -> str:
+        return self.user_name
+
+
 class Post(models.Model):
+    author = models.ForeignKey(Author,on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=50)
     content = models.TextField()
     created_at = models.DateField(null=True)
@@ -10,5 +21,18 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete= models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
     text = models.TextField()
     created_at = models.DateField(null=True)
+
+    def __str__(self) -> str:
+        return f'{self.author}-{self.post}'
+    
+
+class Like(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='ulikes')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='plikes')
+
+    def __str__(self):
+        return f'{self.author} liked {self.post}'
+
